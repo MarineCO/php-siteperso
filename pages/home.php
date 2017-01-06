b69a52a5a250665f0565ffd6897226ac5773d709
marineco <marinecolonge@gmail.com>
2017-01-06 10:12:41 +0100

    nouvel ajout de contenu sur la home

--- pages/home.php
+++ pages/home.php
@@ -1,17 +1,34 @@
-<h1>Accueil Portfolio</h1>
+<div class="punchline">
+	<h2>Jeune développeuse junior motivée, dynamique et curieuse.</h2>
+	<p class="citation">"Lorsqu'on s'occupe d'informatique il faut faire comme les canards... Paraître calme en surface et pédaler comme un forcené par en dessous." Richard LALLEMENT</p>
+	<h2>#InCodeWeTrust</h2>
+</div>
+<div class="bio">
+	<a href="index.php?page=bio">Un peu plus sur moi</a>
+	<a href="#">Télécharger mon CV</a>
+	<a href="#">CV complet</a>
+</div>
 
-<img src="http://placehold.it/150x150" alt="image">
-<h2>Titre</h2>
-<p></p>
-
-<img src="http://placehold.it/150x150" alt="image">
-<h2>Titre</h2>
-<p></p>
-
-<img src="http://placehold.it/150x150" alt="image">
-<h2>Titre</h2>
-<p></p>
-
-<img src="http://placehold.it/150x150" alt="image">
-<h2>Titre</h2>
-<p></p>
+<div class="projet">
+	<h2>Aperçu projets réalisés</h2>
+	
+	<div>
+		<h4>Titre Projet</h4>
+		<img src="http://placehold.it/150x150" alt="image">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+		
+		<h4>Titre Projet</h4>
+		<img src="http://placehold.it/150x150" alt="image">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+		
+		<h4>Titre Projet</h4>
+		<img src="http://placehold.it/150x150" alt="image">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+		
+		<h4>Titre Projet</h4>
+		<img src="http://placehold.it/150x150" alt="image">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+	</div>
+	
+	<a href="#">Pour en voir plus</a>
+</div>
