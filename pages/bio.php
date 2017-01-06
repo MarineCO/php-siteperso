a7faf29b0f77cb7cfa3b4e6b0c42c0b3b56096e9
marineco <marinecolonge@gmail.com>
2017-01-06 10:21:32 +0100

    ajout contenu bio

---
+++ pages/bio.php
@@ -0,0 +1,14 @@
+
+<h2>Web Développeuse</h2>
+
+<img src="#" alt="photo">
+
+<p>Parcours. Résumé en 2 lignes. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur repellat et soluta sapiente pariatur excepturi mollitia quaerat qui, facere, possimus deserunt molestiae atque animi aut commodi suscipit adipisci odio dolor.</p>
+
+<p>Ce que je fais. Résumé en 2 lignes. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo recusandae eius deserunt aperiam quam, iste nostrum corporis dolores est perspiciatis voluptatum praesentium accusamus ipsum nam doloribus magni voluptate commodi laudantium.</p>
+
+<div class="punchline">
+	<h2>Jeune développeuse junior motivée, dynamique et curieuse.</h2>
+	<p class="citation">"Lorsqu'on s'occupe d'informatique il faut faire comme les canards... Paraître calme en surface et pédaler comme un forcené par en dessous." Richard LALLEMENT</p>
+	<h2>#InCodeWeTrust</h2>
+</div>
