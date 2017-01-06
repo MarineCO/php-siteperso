58f62f768c0869db9006ca1adea833818b11ede9
marineco <marinecolonge@gmail.com>
2017-01-06 10:14:17 +0100

    ajout de contenu (formulaire de contact et accès cv)

---
+++ pages/contact.php
@@ -0,0 +1,28 @@
+<div class="contact">
+	<h1>Pour me contacter :</h1>
+	
+	<form method="post" action="#">
+	
+		<label for="prenom">Prénom</label>
+		<input type="text" name="prenom" id="prenom"/>
+	
+		<label for="nom">Nom</label>
+		<input type="text" name="nom" id="nom"/>
+	
+		<label for="email">Adresse e-mail</label>
+		<input type="email" name="email" id="email"/>
+	
+		<label for="tel">Téléphone</label>
+		<input type="text" name="tel" id="tel"/>
+	
+		<label for="message">Message</label>
+		<input type="text" name="message" id="message"/>
+	
+	</form>
+</div>
+
+<div class="cv">
+	<h1>Mon CV :</h1>
+	
+	<a href="#">Mon CV</a>
+</div>
