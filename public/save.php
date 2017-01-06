479ac28c57001c290a923bc94196ec7d01342443
marineco <marinecolonge@gmail.com>
2017-01-06 18:17:18 +0100

    récupération des data

---
+++ public/save.php
@@ -0,0 +1,11 @@
+<?php 
+
+//Récupération des données
+	$firstname = $_POST["firstname"];
+	//var_dump($firstname);
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$tel = $_POST["tel"];
+	$message = $_POST["message"];
+	
+?>
