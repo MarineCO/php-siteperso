5d0583399f8061f906164de895ef24d95983662b
marineco <marinecolonge@gmail.com>
2017-01-06 18:15:50 +0100

    ajout input submit du formulaire et chgment des name / for et id en FR pr EN

--- pages/contact.php
+++ pages/contact.php
@@ -1,13 +1,13 @@
 <div class="contact">
 	<h1>Pour me contacter :</h1>
 	
-	<form method="post" action="#">
+	<form method="post" action="save.php">
 	
-		<label for="prenom">Prénom</label>
-		<input type="text" name="prenom" id="prenom"/>
+		<label for="firstname">Prénom</label>
+		<input type="text" name="firstname" id="firstname"/>
 	
-		<label for="nom">Nom</label>
-		<input type="text" name="nom" id="nom"/>
+		<label for="name">Nom</label>
+		<input type="text" name="name" id="name"/>
 	
 		<label for="email">Adresse e-mail</label>
 		<input type="email" name="email" id="email"/>
@@ -17,7 +17,8 @@
 	
 		<label for="message">Message</label>
 		<input type="text" name="message" id="message"/>
-	
+		
+		<input type="submit" value="Valider">
 	</form>
 </div>
 
