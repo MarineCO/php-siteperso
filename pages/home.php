66da7d64b1d3b5b5700b007d8ac3fd82054670ee
marineco <marinecolonge@gmail.com>
2017-01-06 09:34:19 +0100

    ajout de contenu dans page d'accueil

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,17 @@
 <h1>Accueil Portfolio</h1>
+
+<img src="http://placehold.it/150x150" alt="image">
+<h2>Titre</h2>
+<p></p>
+
+<img src="http://placehold.it/150x150" alt="image">
+<h2>Titre</h2>
+<p></p>
+
+<img src="http://placehold.it/150x150" alt="image">
+<h2>Titre</h2>
+<p></p>
+
+<img src="http://placehold.it/150x150" alt="image">
+<h2>Titre</h2>
+<p></p>
