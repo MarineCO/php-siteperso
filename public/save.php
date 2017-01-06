4f52ec72b11d8ada66ba41a729e9e0902ec316e1
marineco <marinecolonge@gmail.com>
2017-01-06 18:21:58 +0100

    Représentation JSON du message, précédemment récupéré, avec json_encode()

--- public/save.php
+++ public/save.php
@@ -2,10 +2,14 @@
 
 //Récupération des données
 	$firstname = $_POST["firstname"];
-	//var_dump($firstname);
+	// var_dump($firstname);
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$tel = $_POST["tel"];
 	$message = $_POST["message"];
 	
+//Représentation JSON de $message
+	$last_message = json_encode($message);
+	echo $last_message;
+
 ?>
